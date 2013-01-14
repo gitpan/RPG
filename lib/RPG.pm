164d4f30c6df16eda853a7207a769cde7b076846
Peter L. Berghold <cpan@berghold.net>
2013-01-14 17:39:20 +0000

    Import of COWDAWG/RPG-0.5 from CPAN.
    gitpan-cpan-distribution: RPG
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         COWDAWG/RPG-0.5.tar.gz
    gitpan-cpan-author:       COWDAWG
    gitpan-cpan-maturity:     released

---
+++ lib/RPG.pm
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+package RPG;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+RPG
+
+=head1 VERSION
+
+version 0.5
+
+=head1 AUTHOR
+
+Peter L. Berghold <peter@berghold.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Peter L. Berghold.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
